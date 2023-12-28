72d25a2355bd8d4fc42555d7267296069ffc54cc
Suyank Saraswat <suyank.saraswat@gmail.com>
2023-12-28 20:16:06 +0530

    bar chart

--- app/home/_components/Analytics.tsx
+++ app/home/_components/Analytics.tsx
@@ -1,45 +1,19 @@
 import { Box } from '@mui/material';
 import React, { PureComponent, useEffect, useState } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  Rectangle,
+} from 'recharts';
 import { apiFetchAnalytics } from 'src/utils/api-endpoints';
-
-const data = [
-  {
-    name: 'Page A',
-    count: 4000,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    count: 3000,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    count: 2000,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    count: 2780,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    count: 1890,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    count: 2390,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    count: 3490,
-    amt: 2100,
-  },
-];
 
 interface Props {
   searchVal: string;
@@ -72,7 +46,7 @@ export const Analytics = ({ searchVal }: Props) => {
     <Box>Loading...</Box>
   ) : (
     <ResponsiveContainer width="100%" height={300}>
-      <LineChart
+      <BarChart
         width={500}
         height={300}
         data={
@@ -88,10 +62,10 @@ export const Analytics = ({ searchVal }: Props) => {
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="name" />
         <YAxis />
-        <Tooltip />
+        <Tooltip cursor={{ fill: 'transparent' }} />
         <Legend />
-        <Line type="monotone" dataKey="count" stroke="#82ca9d" />
-      </LineChart>
+        <Bar maxBarSize={80} dataKey="count" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
+      </BarChart>
     </ResponsiveContainer>
   );
 };
